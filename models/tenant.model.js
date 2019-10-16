6d85def6691a31671059c5b0745b59e51ef188e0
Brian Kelly <briankelly@bkelbook.attlocal.net>
2019-10-15 21:45:24 -0400

    make it better

--- models/tenant.model.js
+++ models/tenant.model.js
@@ -24,7 +24,7 @@ function validateNewTenant(newTenant) {
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(3).max(255).required(),
         fullName: Joi.string().required(),
-        welcomeMessage: Joi.string.required()
+        welcomeMessage: Joi.string().required()
     }
 
     return Joi.validate(newTenant, schema);
