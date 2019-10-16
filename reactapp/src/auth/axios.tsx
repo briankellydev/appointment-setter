8069ccce11991f904102874bb278f07755dde5f5
Brian Kelly <briankelly@bkelbook.attlocal.net>
2019-10-15 21:14:49 -0400

    make it better

--- reactapp/src/auth/axios.tsx
+++ reactapp/src/auth/axios.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set config defaults when creating the instance
 export const http = axios.create({
-    baseURL: 'https://https://appointment-setter-bk.herokuapp.com/api'
+    baseURL: 'https://appointment-setter-bk.herokuapp.com/api'
 });
 
 http.interceptors.request.use((config) => {
